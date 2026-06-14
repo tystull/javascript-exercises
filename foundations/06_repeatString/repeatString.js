9a8593808df0c9a0e2d2771633eb503d1effaef0
Trish <191945548+tystull@users.noreply.github.com>
2026-06-13 20:05:55 -0700

    Finish 06_repeatString
    A function that takes a string and repeats it num times.
    Copied original string to new string. Uses a loop for a
    duration of num - 1 to concatenate string to newString. This accounts
    for the fact that newString already contains 1 copy of original string.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = string;
 
+    if (num === 0) {
+        return '';
+    }
+    else if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+
+        for (let i = 0; i < num - 1; i++) {
+             newString += string;
+        }
+    }
+
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
