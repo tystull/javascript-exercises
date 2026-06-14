db57ba6423af11c2fab074c33990b66fffb5bf3a
Trish <191945548+tystull@users.noreply.github.com>
2026-06-13 21:12:44 -0700

    Finish 07_reverseString
    Loop starts at largest index of stringand decrements to reverse the
    string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = '';
 
+    for (i = string.length - 1; i >= 0; i--) {
+        reverseString += string.charAt(i);
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
