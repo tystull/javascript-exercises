e6cde68ca3ca1b6b9d3a047870a5176d9751e734
Trish <191945548+tystull@users.noreply.github.com>
2026-06-14 16:08:53 -0700

    Finish 10_leapYears
    If year is divisible by 4 and not divisible by 100, OR
    if year only divisible by 400, it is a  leap year.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    if (!(year % 4) && (year % 100) || !(year % 400)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
