af2a908b2639ff4a66b2fed93d75e85b66184573
Trish <191945548+tystull@users.noreply.github.com>
2026-06-14 13:57:13 -0700

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int1 < 0 ||  
+        int2 < 0 || 
+        typeof int1 != 'number' || 
+        typeof int2 != 'number' ||
+        int1 % 1 != 0 ||
+        int2 % 1 != 0
+        ) {
+        return 'ERROR';
+    }
 
+    let firstNum = int1;
+    let secondNum = int2;
+    if (int1 > int2) {
+        firstNum = int2;
+        secondNum = int1;
+    } 
+
+    let sum = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
