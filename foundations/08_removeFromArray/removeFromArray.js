e4805b48074cc0b68ea90c2bcd5037b73f4bbcd6
Trish <191945548+tystull@users.noreply.github.com>
2026-06-14 00:46:33 -0700

    Finish 08_removeFromArray
    Decrement i as item is being removed to keep array index accurate.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, itemToRemove) {
+
+   // If item equals one of the item arguments, remove it
+   // must remove 3, 2 from 1 3 4
+    for (let i = 0; i < array.length; i++) {
+        console.log('before: ' + array);
+        for (let j = 1; j < arguments.length; j ++) {
+            if (array[i] === arguments[j]) {
+                console.log('removed: ' + arguments[j]);
+                array.splice(i, 1);
+                console.log('after: ' + array);
+                // Must subtract i for count to remain accuratebecause item being removed
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
