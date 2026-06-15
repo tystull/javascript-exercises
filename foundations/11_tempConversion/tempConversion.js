cef553307c309ce4b115cdb3a0adca59488c65dd
Trish <191945548+tystull@users.noreply.github.com>
2026-06-14 21:38:35 -0700

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Fahrenheit to Celsius conversion: (x - 32) * (5/9)
+  celsius = (fahrenheit - 32) * (5/9);
+
+  // Round to one decimal place
+  return Math.round(celsius * 10) / 10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Celsius to Fahrenheit conversion: (x * (9/5) + 32)
+  fahrenheit = (celsius * (9/5) + 32);
+
+  // Round to one decimal place
+  return Math.round(fahrenheit * 10) / 10;
+
 };
 
 // Do not edit below this line
